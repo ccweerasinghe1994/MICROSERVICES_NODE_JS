66764af3c5a9a4ef4ca8ce79f27b50a7b6f62d69
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-06-17 07:48:55 +0530

    feat: Implement posts service with Express and CRUD functionality

---
+++ 02_A_Mini_Microservices_App/posts/src/index.ts
@@ -0,0 +1,28 @@
+import express, { RequestHandler } from 'express';
+import { randomBytes } from 'node:crypto';
+
+const posts:Record<string, { id: string, title: string }> = {}
+
+const app = express();
+app.use(express.json());
+const requestHandler:RequestHandler = (req, res) => {
+    res.json(Object.values(posts));
+}
+
+app.get("/posts", requestHandler);
+
+
+const createPostHandler:RequestHandler = (req, res) => {
+    const postId = randomBytes(4).toString('hex');
+    const post = req.body;
+    posts[postId] = { id: postId, title: post.title };
+    res.status(201).json({ message: "Post created", post });
+}   
+    
+app.post("/posts", createPostHandler);
+
+app.listen(4000, () => {
+    console.log("Posts service is running on port 4000");
+});
+
+
